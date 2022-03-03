15ea1c6695649bf76db61e55d993b0e8c963a643
barrett <yicheng.tan@seraphim-energy.com>
2022-03-03 22:14:52 +0800

    feat: 创建simple工程，运行项目

--- bdf3-parent/bdf3-autoconfigure/src/main/java/com/bstek/bdf3/autoconfigure/cola/ColaAutoConfiguration.java
+++ bdf3-parent/bdf3-autoconfigure/src/main/java/com/bstek/bdf3/autoconfigure/cola/ColaAutoConfiguration.java
@@ -1,10 +1,10 @@
 package com.bstek.bdf3.autoconfigure.cola;
 
+import com.bstek.bdf3.cola.configure.ColaConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import com.bstek.bdf3.cola.configure.ColaConfiguration;
 
 /**
  * @author Kevin Yang (mailto:kevin.yang@bstek.com)
